9eb88540e483983567c33f8fea33872a210ef28f
dnfebri <dwinofebri@gmail.com>
2023-02-08 12:44:43 +0700

    solved

--- src/components/Layout/LayoutMain.tsx
+++ src/components/Layout/LayoutMain.tsx
@@ -1,8 +1,9 @@
 import { Box, BoxProps, Container } from "@chakra-ui/react";
 import { Navbar } from "components";
+import { Footer } from "components/Footer";
 import React from "react";
 
-interface LayoutMainProps extends BoxProps {}
+interface LayoutMainProps extends BoxProps { }
 
 export const LayoutMain: React.FC<LayoutMainProps> = props => {
   return (
@@ -11,6 +12,7 @@ export const LayoutMain: React.FC<LayoutMainProps> = props => {
       <Container pt={{ base: "20", md: "40" }} maxW="container.xl">
         {props.children}
       </Container>
+      <Footer />
     </Box>
   );
 };
