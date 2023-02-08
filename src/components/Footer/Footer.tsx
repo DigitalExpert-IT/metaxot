b270dc9cced6ab9cafbe02250b98c77ffa5d632a
dnfebri <dwinofebri@gmail.com>
2023-02-08 12:44:43 +0700

    solved

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  Container,
   HStack,
   Image,
   Input,
@@ -19,48 +20,54 @@ export const Footer = () => {
   const { t } = useTranslation();
   return (
     <Box bg={"gray.700"}>
-      <SimpleGrid
-        columns={{ base: 1, md: 2 }}
-        spacing={12}
-        px={{ base: 4, md: "14" }}
-        py={"14"}
-      >
-        <Box maxW={"sm"} mx={{ base: "auto", md: 0 }}>
-          <Text fontSize={"4xl"} fontWeight={"black"}>
-            METAXOT
-          </Text>
-          <VStack spacing={4} align={"start"} mt={"4"}>
-            <Text>{t("common.footer.dropYourEmail")}</Text>
-            <Input type={"email"} variant={"outline"} />
-            <Button colorScheme={"metaxot"}>{t("common.send")}</Button>
-          </VStack>
-        </Box>
-        <Wrap
-          ml={"8"}
-          pt={"4"}
-          justify={{ base: "center", md: "end" }}
-          alignItems={"start"}
-          spacing={8}
-          w={"full"}
-          mx={{ base: "auto", md: 0 }}
+      <Container maxW="container.xl">
+        <SimpleGrid
+          columns={{ base: 1, md: 2 }}
+          spacing={"12"}
+          px={"4"}
+          py={"14"}
         >
-          {FOOTER_NAVIGATION.map((row, i) => (
-            <FooterNav key={i} data={row} />
-          ))}
-        </Wrap>
-        <HStack spacing={{ base: "4", sm: "8" }} mx={{ base: "auto", md: 0 }}>
-          {SOSMED_FOOTER.map((row, i) => (
-            <Box key={i}>
-              <Link href={row.href} target="_blank">
-                <Image src={row.icon} alt={row.href} h={"7"} />
-              </Link>
-            </Box>
-          ))}
-        </HStack>
-        <Box textAlign={{ base: "center", md: "end" }}>
-          <Text>2023 Metaxot. {t("common.footer.allRightsReserved")}</Text>
-        </Box>
-      </SimpleGrid>
+          <Box maxW={"sm"} mx={{ base: "auto", md: 0 }}>
+            <Text fontSize={"4xl"} fontWeight={"black"}>
+              METAXOT
+            </Text>
+            <VStack spacing={4} align={"start"} mt={"4"}>
+              <Text>{t("common.footer.dropYourEmail")}</Text>
+              <Input type={"email"} variant={"outline"} />
+              <Button colorScheme={"metaxot"}>{t("common.send")}</Button>
+            </VStack>
+          </Box>
+          <Wrap
+            ml={"8"}
+            pt={"4"}
+            justify={{ base: "center", md: "end" }}
+            alignItems={"start"}
+            spacing={8}
+            w={"full"}
+            mx={{ base: "auto", md: 0 }}
+          >
+            {FOOTER_NAVIGATION.map((row, i) => (
+              <FooterNav key={i} data={row} />
+            ))}
+          </Wrap>
+          <HStack spacing={{ base: "4", sm: "8" }} mx={{ base: "auto", md: 0 }}>
+            {SOSMED_FOOTER.map((row, i) => (
+              <Box key={i}>
+                <Link href={row.href} target="_blank">
+                  <Image src={row.icon} alt={row.href} h={"7"} />
+                </Link>
+              </Box>
+            ))}
+          </HStack>
+          <Box textAlign={{ base: "center", md: "end" }}>
+            <Text>
+              {new Date().getFullYear() +
+                " Metaxot. " +
+                t("common.footer.allRightsReserved")}
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Container>
     </Box>
   );
 };
