f42d49b9790964930efa134c50058d1390cda660
Manan <manan.jabrik@gmail.com>
2023-02-09 14:35:50 +0700

    Section Header Pages (#17)
    * Section Header Pages [WIP]
    
    * create animation
    
    * fix overflow x
    
    * setup content
    
    * Removed code unesed locales
    
    * first commit
    
    * fix: ui footer
    
    * solved
    
    * solved pading navigation
    
    * add size button to small, and add input to small size
    
    * fix: pading navigation
    
    * fix: pading dekstop
    
    * solved drop email footer
    
    * solved bg color
    
    * fix: mobile scale
    
    * fix: overflow circle
    
    * update
    
    * update
    
    * fix animation
    
    * add font weight
    
    * refoldering pages
    
    * update
    
    * fix import component
    
    * singgle image
    
    ---------
    
    Co-authored-by: dnfebri <dwinofebri@gmail.com>
    Co-authored-by: Y0SH4 <yosa.agung13@gmail.com>

---
+++ src/components/pages/Home/SectionHeader.tsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Text,
+  Image,
+  Stack,
+  Button,
+  HStack,
+  Heading,
+} from "@chakra-ui/react";
+import { Trans } from "react-i18next";
+import { CircleGalaxy } from "components";
+
+export const SectionHeader = () => {
+  return (
+    <HStack justifyContent="space-between" alignItems={"center"}>
+      <CircleGalaxy />
+      <Stack spacing={8} zIndex="4">
+        <Heading
+          size={{ md: "3xl", base: "2xl" }}
+          lineHeight={{ md: "4.5rem", base: "1" }}
+          fontWeight="600"
+        >
+          <Trans
+            components={{ br: <br /> }}
+            i18nKey={"pages.home.header.title"}
+          />
+        </Heading>
+        <Text>
+          <Trans
+            components={{ br: <br /> }}
+            i18nKey={"pages.home.header.subtitle"}
+          />
+        </Text>
+        <Box>
+          <Button colorScheme="metaxot">Download Metaxot</Button>
+        </Box>
+      </Stack>
+      <Stack display={{ lg: "flex", md: "none", base: "none" }}>
+        <Image
+          zIndex={2}
+          rounded={"full"}
+          alt="masking-circle"
+          src="/assets/illustration/masking_circle.svg"
+          position="relative"
+          bg="black"
+        />
+      </Stack>
+    </HStack>
+  );
+};
