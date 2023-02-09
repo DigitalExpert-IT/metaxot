840b6a222c7ae333de723ae712f69e9c190e7da7
mananjabrik <manan.jabrik@gmail.com>
2023-02-09 14:30:18 +0700

    singgle image

--- src/components/pages/Home/SectionHeader.tsx
+++ src/components/pages/Home/SectionHeader.tsx
@@ -43,7 +43,7 @@ export const SectionHeader = () => {
           src="/assets/illustration/masking_circle.svg"
           position="relative"
           bg="black"
-        ></Image>
+        />
       </Stack>
     </HStack>
   );
