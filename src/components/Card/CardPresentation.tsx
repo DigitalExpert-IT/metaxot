818f03bfeb3d7aa34ffbcfcd899f90897ac23a05
dnfebri <dwinofebri@gmail.com>
2023-02-14 12:20:56 +0700

    resolve props naming

--- src/components/Card/CardPresentation.tsx
+++ src/components/Card/CardPresentation.tsx
@@ -2,11 +2,11 @@ import { Stack, StackProps, Text } from "@chakra-ui/react";
 import React from "react";
 import { Trans } from "react-i18next";
 
-type Props = StackProps & {
+type TCardPresentation = StackProps & {
   subtitle?: string;
 };
 
-export const CardPresentation = (props: Props) => {
+export const CardPresentation: React.FC<TCardPresentation> = props => {
   const { subtitle, ...rest } = props;
   return (
     <Stack pt={"28"} pb={"16"} mx={"auto"} {...rest}>
