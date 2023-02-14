72c6c78d30099f0cc342ffd2202fc526469510d1
mananjabrik <manan.jabrik@gmail.com>
2023-02-15 00:39:59 +0700

    update

--- src/components/pages/Home/SectionRoadMap.tsx
+++ src/components/pages/Home/SectionRoadMap.tsx
@@ -87,7 +87,7 @@ export const SectionRoadmap = () => {
           </Stack>
         </Stack>
       </Stack> */}
-      <Stack>
+      <Stack py="16">
         <TimeLine />
       </Stack>
     </Stack>
