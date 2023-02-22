55f2eab71d92e9b2e4392c4a89ea636920df324e
mananjabrik <manan.jabrik@gmail.com>
2023-02-22 14:54:46 +0700

    setup title pages

--- src/components/Layout/LayoutMain.tsx
+++ src/components/Layout/LayoutMain.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import { Navbar } from "components";
 import { Footer } from "components/Footer";
 import { Box, BoxProps, Container } from "@chakra-ui/react";
+import Head from "next/head";
 
-export const LayoutMain: React.FC<BoxProps> = props => {
+interface LayoutMainProps extends BoxProps {
+  title: string;
+}
+
+export const LayoutMain: React.FC<LayoutMainProps> = props => {
+  const { title } = props;
   return (
     <Box>
+      <Head>
+        <title>{title}</title>
+      </Head>
       <Navbar />
       <Container pt={{ base: "20", md: "40" }} maxW="container.xl">
         {props.children}
