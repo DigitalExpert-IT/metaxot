d508bb4cc46d8bdb7fc9a047bfd7fa7610eece8b
Manan <manan.jabrik@gmail.com>
2023-11-01 17:27:44 +0700

    Market NFT [Example Design Mockup] (#38)
    * feat market [WIP]
    
    * feat update category
    
    * feat detail NFT
    
    * update detail
    
    * feat mobile design setup

---
+++ src/constant/pages/category.ts
@@ -0,0 +1,6 @@
+export const CATEGORY = [
+  { name: "all", isActive: true },
+  { name: "caracters", isActive: false },
+  { name: "lands", isActive: false },
+  { name: "materials", isActive: false },
+];
