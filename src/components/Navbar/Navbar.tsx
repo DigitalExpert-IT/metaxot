a423f85498e13a84d84119e63694d9f93afd9261
mananjabrik <manan.jabrik@gmail.com>
2023-11-02 09:58:39 +0700

    fix: undefined host

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,13 +5,19 @@ import {
   NavbarBrandDrawer,
   NavbarItemWrapper,
 } from "./NavbarUtils";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NAVIGATION } from "constant";
 import { Flex, useDisclosure } from "@chakra-ui/react";
 import { ConnectWallet, darkTheme } from "@thirdweb-dev/react";
 
 export const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [host, setHost] = useState("");
+  useEffect(() => {
+    if (window.location.host) {
+      setHost(window.location.host);
+    }
+  }, []);
   return (
     <NavbarMain isOpen={isOpen}>
       <NavbarBody>
@@ -44,7 +50,7 @@ export const Navbar = () => {
             modalSize={"wide"}
             welcomeScreen={{
               img: {
-                src: `http://${window.location.host}/assets/logo/metaxot.svg`,
+                src: `http://${host}/assets/logo/metaxot.svg`,
                 width: 150,
                 height: 150,
               },
