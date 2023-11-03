c4b581d81665d2a315db2230645be79450618650
mananjabrik <manan.jabrik@gmail.com>
2023-11-03 12:17:51 +0700

    integrate setup

---
+++ src/pages/api/market/list/pre-mint/index.ts
@@ -0,0 +1,24 @@
+import { NextApiHandler } from "next";
+
+const DUMMY_JSON = {
+  "550e8400-e29b-41d4-a716-446655440000": {
+    name: "land",
+    latitude: "40.741895",
+    longitude: "-73.989308",
+    picture: "http://localhost:3000/assets/content/land/building.png",
+    bg: "http://localhost:3000/assets/content/land/bg.png",
+  },
+  "550e8400-e29b-41d4-a716-446655445000": {
+    name: "land",
+    latitude: "40.741895",
+    longitude: "-73.989308",
+    picture: "http://localhost:3000/assets/content/land/building.png",
+    bg: "http://localhost:3000/assets/content/land/bg.png",
+  },
+};
+
+const handler: NextApiHandler = (req, res) => {
+  return res.status(200).json(DUMMY_JSON);
+};
+
+export default handler;
