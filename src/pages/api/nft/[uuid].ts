5b276c8f75b6f4c6322cf676e3bc13318710cece
mananjabrik <manan.jabrik@gmail.com>
2023-11-23 16:24:01 +0700

    feat: CRUD NFT

--- src/pages/api/nft/[uuid].ts
+++ src/pages/api/nft/[uuid].ts
@@ -1,25 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiHandler } from "next";
+import { findOne, remove, update } from "services";
 
 const prisma = new PrismaClient();
 
 const handler: NextApiHandler = async (req, resp) => {
   const querId = req.query.uuid?.toString();
-  try {
-    const data = await prisma.eRC721.findFirst({
-      where: {
-        uuid: querId,
-      },
-      include: {
-        attributes: {},
-      },
-    });
-    if (data) {
-      return resp.status(200).json(data);
-    }
-    resp.status(400).json({ message: "undifined" });
-  } catch (e: any) {
-    return resp.status(500).json({ ...e });
+  if (!querId) return;
+
+  if (req.method === "GET") {
+    findOne(prisma, querId, resp);
+  }
+
+  if (req.method === "PATCH") {
+    update(prisma, querId, req.body, resp);
+  }
+
+  if (req.method === "DELETE") {
+    remove(prisma, querId, resp);
   }
 };
 export default handler;
--- src/pages/api/nft/index.ts
+++ src/pages/api/nft/index.ts
@@ -1,22 +1,22 @@
-import { PrismaClient } from "@prisma/client";
 import { NextApiHandler } from "next";
+import { PrismaClient } from "@prisma/client";
+import { create, findAll } from "services";
 
 const prisma = new PrismaClient();
 
 const handler: NextApiHandler = async (req, res) => {
-  try {
-    const allNFTs = await prisma.eRC721.findMany({
-      include: {
-        attributes: {},
-      },
-    });
-    res.status(200).json(allNFTs);
-  } catch (e: any) {
-    res.status(500).json({
-      errorCode: e.errorCode,
-      message: e.message,
-    });
+  if (req.method === "GET") {
+    await findAll(prisma, res);
   }
+
+  if (req.method === "POST") {
+    await create(prisma, req, res);
+  }
+
+  res.status(405).json({
+    status: 405,
+    message: "method not allowed",
+  });
 };
 
 export default handler;
