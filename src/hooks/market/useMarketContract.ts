fac8f4b2d0caa0ccbed8458ab0531185cee87e2f
vickoeff <vic.eff3@gmail.com>
2024-01-24 09:47:48 +0700

    revert manually add artifacts contract

--- src/hooks/market/useMarketContract.ts
+++ src/hooks/market/useMarketContract.ts
@@ -1,6 +1,6 @@
 import { useContract } from "@thirdweb-dev/react";
 import { MARKET_CONTRACT } from "constant/address";
-import Market from "contracts/artifacts/contracts/Market.sol/Market.json";
+import Market from "metaxot-contract/artifacts/contracts/Market.sol/Market.json";
 
 const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID ?? "0x29a";
 const address = MARKET_CONTRACT[CHAIN_ID as "0x29a"];
--- src/hooks/xpc/useXpcContract.ts
+++ src/hooks/xpc/useXpcContract.ts
@@ -1,7 +1,7 @@
 import { useContract } from "@thirdweb-dev/react";
 import { XPC_CONTRACT } from "constant/address";
 
-import XPC from "contracts/artifacts/contracts/Xpc.sol/Xpc.json";
+import XPC from "metaxot-contract/artifacts/contracts/Xpc.sol/Xpc.json";
 const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID ?? "0x29a";
 const address = XPC_CONTRACT[CHAIN_ID as "0x29a"];
 
